154ad6175aab5101aba73e8110681c373212e011
tkm11 <document.t1128@gmail.com>
2020-04-21 13:05:41 +0900

    Modefied templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_lite(request):
-    return render(request, 'blog/post_lite.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_lite.html', {'posts': posts})
+
